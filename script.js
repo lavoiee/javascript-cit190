2a6b04f119b1860ad13d3223eeb6a60304d7037d
Eric Lavoie <Eric Lavoie>
2017-01-19 17:49:50 -0500

    It works

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("Hello World!!!");
+/*window.alert("Hello World!!!");*/
+// add event listener for button
+// click is the event itself. onclick is the eventhandler
+document
+    .getElementById("buyNowButton")
+    .addEventListener("click", buyNow); //args 1 event, 2 what to do with it (function)
+
+function buyNow(){
+    var firstname = 
+        document.getElementById("firstname").value;
+    alert("Thanks for the money! " + firstname);
+}
